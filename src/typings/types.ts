abb118f0d3ac85b0c9d7820b8543e552c9955e74
vanyadelova <happy.vanya.delova@gmail.com>
2019-07-25 12:57:13 +0200

    exporting interface

---
+++ src/typings/types.ts
@@ -0,0 +1,12 @@
+export interface MatchMeta {
+  id: number
+  awayName: string
+  group: string
+  createdAt: string
+  homeName: string
+  name: string
+  objectId: string
+  sport: string
+  country: string
+  state: string
+}
